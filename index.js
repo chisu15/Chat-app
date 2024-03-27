3eaaf7c7c723743a1c0cc2b8e3f6082223744a35
chisu15 <trunghieudo1509@gmail.com>
2024-03-27 17:09:21 +0700

    1

---
+++ index.js
@@ -0,0 +1,99 @@
+const {
+    on
+} = require('events');
+const express = require('express');
+const http = require('http');
+const {
+    disconnect
+} = require('process');
+const socketio = require('socket.io');
+const fs = require('fs-extra');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+var userList = [];
+var messageHistory = [];
+app.use(express.static("public"));
+app.set('views', './view');
+const port = 3000;
+try {
+    messageHistory = fs.readJsonSync('./message.json');
+    // userList = fs.readJsonSync('./userList.json');
+} catch (error) {
+    console.error('Error while reading JSON file:', error);
+}
+
+
+io.on('connection', (socket) => {
+    socket.on('userInfo', data => {
+        if (userList.indexOf(data) >= 0) {
+            socket.emit('loginFail');
+        } else {
+            console.log("Socket userInfo", data);
+            userList.push(data);
+            socket.username = data;
+            socket.emit("loginSuccess", data)
+            io.emit('users', userList)
+        }
+    })
+    socket.on('chatMessage', data => {
+
+        var message = {
+            room: socket.room,
+            username: socket.username,
+            msg: data.msg
+        }
+        if (socket.username && socket.room) {
+            console.log("LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL",message)
+            messageHistory.push(message);
+            if(socket.username == data.username)
+            {
+                
+            }
+        }
+        io.to(socket.room).emit('sendMessage', message)
+    })
+    socket.on('createRoom', data => {
+        socket.join(data);
+        socket.room = data;
+        console.log(socket.adapter.rooms);
+        var roomList = [];
+        for (room of socket.adapter.rooms) {
+            roomList.push(room[0])
+        }
+        console.log('Socket createRoom', data);
+        console.log(roomList);
+        io.emit('listRoom', roomList);
+    })
+
+    socket.on('clickToChat', data => {
+        // socket.leave(socket.room);
+        console.log("Socket clickToChat", data, socket.room);
+        socket.join(data);
+        socket.room = data;
+        socket.emit('moveToChat', data);
+        // io.to(data).emit('sendMessage', messageHistory)
+
+    });
+
+    socket.on('logout', () => {
+        userList.splice(
+            userList.indexOf(socket.username), 1
+        );
+        socket.broadcast.emit('users', userList);
+        fs.writeJsonSync('./message.json',messageHistory)
+    })
+
+});
+
+app.get('/', (req, res) => {
+    const {
+        username
+    } = req.query;
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
---
+++ public/js/script.js
@@ -0,0 +1,149 @@
+var socket = io();
+var username;
+var yourName;
+const container = document.querySelector('.container');
+const login = document.querySelector('.login');
+const loginForm = document.querySelector('#login-form');
+const logoutForm = document.querySelector('#logout-form');
+const createRoomForm = document.querySelector("#create-room")
+const chatUsers = document.querySelector('.user-list')
+
+let roomName
+chatUsers.addEventListener('click', (e)=>{
+    if(e.target && e.target.classList.contains('user')){
+        var users = document.querySelectorAll('.user')
+        users.forEach(u =>{
+            u.classList.remove('active')
+        })
+        e.target.classList.add('active')
+        var name = e.target.textContent;
+        console.log(name);
+        socket.emit('clickToChat', name);
+    }
+})
+socket.on('moveToChat', data=>{
+    createChatBox(data);
+})
+loginForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const inputValue = document.getElementById('username').value;
+    username = inputValue;
+    socket.emit('userInfo', inputValue)
+});
+
+logoutForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    socket.emit('logout');
+    container.style.display = 'none'
+    login.style.display = 'flex'
+})
+
+const chatForm = document.querySelector('.send-area');
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const msg = e.target.elements.msg.value;
+    if (msg != '') {
+        socket.emit('chatMessage',{username: yourName ,msg: msg});
+    }
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+})
+
+createRoomForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const roomName = e.target.roomName.value;
+    if(roomName)
+    {
+        socket.emit('createRoom', roomName);
+    }
+    e.target.roomName.value =''
+})
+
+socket.on('sendMessage', data => {
+    outputMessage(data);
+    scrollToBottom()
+})
+socket.on('loginFail', () => {
+    alert("Login failed")
+})
+
+socket.on("loginSuccess", data => {
+    yourName = data;
+    addYou(data);
+    container.style.display = 'flex'
+    login.style.display = 'none'
+})
+socket.on('users', (users) => {
+    console.log(socket.username);
+    console.log(userList);
+    console.log(1);
+    renderUserList(users)
+})
+socket.on('listRoom',roomList=>{
+    console.log(roomList);
+    renderRoomList(roomList)
+})
+function createChatBox(userName) {
+    var chatArea = document.querySelector('.chat-area');
+    var titleChat = document.querySelector('.title-chat');
+    var msgArea = document.querySelector('.msg-area');
+    titleChat.textContent = userName;
+    msgArea.innerHTML = '';
+}
+
+function scrollToBottom() {
+    var chatArea = document.querySelector('.msg-area');
+    chatArea.scrollTop = chatArea.scrollHeight;
+}
+
+function outputMessage(message) {
+
+    var chatArea = document.querySelector('.msg-area')
+    var side = (yourName === message.username) ? 'you' : 'away';
+    var messageEle;
+    if (side == 'away') {
+        messageEle =
+            `<ul class="user-chat away">
+        <h3 class="user-name">${message.username}</h3>
+        <li class="message">
+        ${message.msg}
+        </li>
+    </ul>`
+    } else {
+        messageEle =
+            `<ul class="user-chat you">
+        <li class="message">
+        ${message.msg}
+        </li>
+        <h3 class="user-name">${message.username}</h3>
+    </ul>`
+    }
+    chatArea.innerHTML += messageEle;
+}
+
+function addYou(user) {
+    var you = document.querySelector('.you-online')
+    you.innerHTML = `👩🏻‍💻 ${user} `
+}
+var userList = document.querySelector('.user-list');
+const userActive = document.querySelectorAll('.user');
+console.log(userActive);
+
+function renderUserList(userList) {
+    let userListElement = document.querySelector('.online');
+    userListElement.innerHTML = ""
+
+    userList.forEach(user => {
+        var userOnline = `<li class="user">${user}</li>`;
+        userListElement.innerHTML += userOnline;
+    });
+}
+function renderRoomList(roomList) {
+    let roomListElement = document.querySelector('.room');
+    roomListElement.innerHTML = ""
+
+    roomList.forEach(room => {
+        var roomOnline = `<li class="user">${room}</li>`;
+        roomListElement.innerHTML += roomOnline;
+    });
+}
